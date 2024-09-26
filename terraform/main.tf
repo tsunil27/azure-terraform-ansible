c14d4aacc02b29c03c81d20addf1208e7a559964
tsunil27 <159771993+tsunil27@users.noreply.github.com>
2024-09-27 02:18:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,3 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "ae6e5027-d51b-4ead-b090-1055273702f8"   # Use a variable for flexibility
-}
-
-data "azurerm_subscription" "current" {}
-
-output "subscription_name" {
-  value = data.azurerm_subscription.current.display_name
-}
-
-output "subscription_id" {
-  value = data.azurerm_subscription.current.subscription_id
-}
-
-# Add this section to fetch the resource group
-data "azurerm_resource_group" "example" {
-  name = "stanuku-rg"  # Replace with your resource group's name
-}
-
 output "resource_group_name" {
-  value = data.azurerm_resource_group.example.name
-}
-
-output "resource_group_id" {
-  value = data.azurerm_resource_group.example.id
+  value = azurerm_resource_group.example.name  # Adjust according to your resource definition
 }
