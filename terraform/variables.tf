ebe3766c6787648fe35c29d32c3bed5ba4858155
tsunil27 <159771993+tsunil27@users.noreply.github.com>
2024-09-27 19:24:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,33 @@
-variable "subscription_id" {
-  description = "ae6e5027-d51b-4ead-b090-1055273702f8"
-  type        = string
+variable "resource_group_name" {
+  description = "stanuku-rg"
+  default     = "stanuku-rg"
+}
+
+variable "location" {
+  default     = "eastus"
+  description = "Location where resources will be created"
+}
+
+variable "tags" {
+  description = "Map of the tags to use for the resources that are deployed"
+  type        = map(string)
+  default = {
+    environment = "codelab"
+  }
+}
+
+variable "application_port" {
+  description = "Port that you want to expose to the external load balancer"
+  default     = 80
+}
+
+variable "admin_user" {
+  description = "User name to use as the admin account on the VMs that will be part of the VM scale set"
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Default password for admin account"
+  default     = "ChangeMe123!"
+  sensitive   = true
 }
