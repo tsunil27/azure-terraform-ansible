06b47fe12fe75f7d38a6886403fe1df99c16d04e
tsunil27 <159771993+tsunil27@users.noreply.github.com>
2024-09-27 19:23:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,79 +1,209 @@
-# Configure the Azure provider
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
-# Create a Resource Group
-resource "azurerm_resource_group" "main" {
-  name     = "myResourceGroup"
-  location = "East US"
+resource "azurerm_resource_group" "vmss" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
 
-# Create a Virtual Network
-resource "azurerm_virtual_network" "main" {
-  name                = "myVnet"
+resource "random_string" "fqdn" {
+  length  = 6
+  special = false
+  upper   = false
+  number  = false
+}
+
+resource "azurerm_virtual_network" "vmss" {
+  name                = "vmss-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  tags                = var.tags
 }
 
-# Create a Subnet
-resource "azurerm_subnet" "main" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_subnet" "vmss" {
+  name                 = "vmss-subnet"
+  resource_group_name  = azurerm_resource_group.vmss.name
+  virtual_network_name = azurerm_virtual_network.vmss.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create a Public IP Address
-resource "azurerm_public_ip" "main" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+resource "azurerm_public_ip" "vmss" {
+  name                = "vmss-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  allocation_method   = "Static"
+  domain_name_label   = random_string.fqdn.result
+  tags                = var.tags
 }
 
-# Create a Network Interface
-resource "azurerm_network_interface" "main" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_lb" "vmss" {
+  name                = "vmss-lb"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
-  }
-}
-
-# Create a Virtual Machine
-resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "myVM"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-  admin_password        = "Password1234!"  # Secure this password properly in production
-
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.vmss.id
   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+  tags = var.tags
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  loadbalancer_id = azurerm_lb.vmss.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "vmss" {
+  resource_group_name = azurerm_resource_group.vmss.name
+  loadbalancer_id     = azurerm_lb.vmss.id
+  name                = "ssh-running-probe"
+  port                = var.application_port
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = azurerm_resource_group.vmss.name
+  loadbalancer_id                = azurerm_lb.vmss.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = var.application_port
+  backend_port                   = var.application_port
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.vmss.id
+}
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+  name                = "vmscaleset"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = var.admin_user
+    admin_password       = var.admin_password
+    custom_data          = file("web.conf")
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = azurerm_subnet.vmss.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      primary                                = true
+    }
+  }
+
+  tags = var.tags
 }
 
-# Output the Public IP of the Virtual Machine
-output "public_ip_address" {
-  value = azurerm_public_ip.main.ip_address
+resource "azurerm_public_ip" "jumpbox" {
+  name                = "jumpbox-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  allocation_method   = "Static"
+  domain_name_label   = "${random_string.fqdn.result}-ssh"
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "jumpbox" {
+  name                = "jumpbox-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+
+  ip_configuration {
+    name                          = "IPConfiguration"
+    subnet_id                     = azurerm_subnet.vmss.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jumpbox.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_virtual_machine" "jumpbox" {
+  name                  = "jumpbox"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.vmss.name
+  network_interface_ids = [azurerm_network_interface.jumpbox.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "jumpbox-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "jumpbox"
+    admin_username = var.admin_user
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = var.tags
 }
