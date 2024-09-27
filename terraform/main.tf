a9a7821edfb0b1599858fa77193770b7c1d77b3f
tsunil27 <159771993+tsunil27@users.noreply.github.com>
2024-09-27 18:30:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,79 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name  # Adjust according to your resource definition
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
 }
- 
+
+# Create a Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a Public IP Address
+resource "azurerm_public_ip" "main" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
+# Create a Network Interface
+resource "azurerm_network_interface" "main" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# Create a Virtual Machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "myVM"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  size                  = "Standard_DS1_v2"
+  admin_username        = "adminuser"
+  admin_password        = "Password1234!"  # Secure this password properly in production
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+# Output the Public IP of the Virtual Machine
+output "public_ip_address" {
+  value = azurerm_public_ip.main.ip_address
+}
